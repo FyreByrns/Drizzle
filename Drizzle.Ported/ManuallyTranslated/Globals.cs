980ddfe3b41fa9b4cd050261defde76310ca9767
Gavin White <horace5413@gmail.com>
2021-08-16 10:08:18 -0700

    move some globals over

---
+++ Drizzle.Ported/ManuallyTranslated/Globals.cs
@@ -0,0 +1,9 @@
+using System;
+using Drizzle.Lingo.Runtime;
+
+namespace Drizzle.Ported {
+    public sealed partial class MovieScript {
+        [LingoGlobal] public LingoRect[] lightRects;
+        [LingoGlobal] public LingoNumber fac;
+    }
+}
